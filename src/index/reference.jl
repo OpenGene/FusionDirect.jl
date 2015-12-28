a21fba93020225eeca54e1d44620ab0d55d4b4fa
Shifu Chen <chen@haplox.com>
2015-12-28 13:54:05 +0800

    use AbstractString for filename

--- src/index/reference.jl
+++ src/index/reference.jl
@@ -1,4 +1,4 @@
-function load_ref(ref_file::ASCIIString, chr::ASCIIString, from::Int64, to::Int64)
+function load_ref(ref_file::AbstractString, chr::AbstractString, from::Int64, to::Int64)
     io = fasta_open(ref_file)
     while (fa = fasta_read(io))!=false
         if fa.name == chr

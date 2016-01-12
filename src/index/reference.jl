735bbd0122ee1cfd4653a2fd769d601e12772722
Shifu Chen <chen@haplox.com>
2016-01-12 22:48:15 +0800

    add load_reference func

--- src/index/reference.jl
+++ src/index/reference.jl
@@ -9,3 +9,32 @@ function load_chr(ref_file::AbstractString, chr::AbstractString)
     end
     return false
 end
+
+function load_single_file_reference(ref_file::AbstractString, ref::Dict{AbstractString, Sequence})
+    io = fasta_open(ref_file)
+    while (fa = fasta_read(io))!=false
+        ref[fa.name] = fa.sequence
+    end
+end
+
+function load_folder_reference(ref_folder::AbstractString, ref::Dict{AbstractString, Sequence})
+    files = readdir(ref_folder)
+    for file in files
+        if contains(file, ".fa") || contains(file, ".fa.gz")
+            load_single_file_reference(joinpath(ref_folder, file), ref)
+        end
+    end
+end
+
+# load the whold reference to a Dict{AbstractString, Sequence}
+# the ref_path can be either a single reference fa file, or a folder contains reference files
+# all reference files can be gzipped
+function load_reference(ref_path::AbstractString)
+    ref = Dict{AbstractString, Sequence}()
+    if isdir(ref_path)
+        load_folder_reference(ref_path, ref)
+    else
+        load_single_file_reference(ref_path, ref)
+    end
+    return ref
+end
